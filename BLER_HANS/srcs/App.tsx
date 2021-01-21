ea0049a9b8d8ae1cb7662627c3d571228939bc63
seo2im <seo2im6492@gmail.com>
2021-01-21 16:08:46 +0900

    api testing

--- BLER_HANS/srcs/App.tsx
+++ BLER_HANS/srcs/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import Reducer from './Modules'
 const store = createStore(Reducer);
+
+import key from '../secret'
+
+import axios from 'axios';
 
 import {
   View,
@@ -10,6 +14,19 @@ import {
 } from 'react-native';
 
 const App = () => {
+  const [test, setTest] = useState([]);
+
+  useEffect(() => {
+    axios({
+      url : 'https://open-api.bser.io/v1/data/hash',
+      method : 'get',
+      headers : {
+        'accept' : 'application/json',
+        'x-api-key' : key
+      }
+    }).then(res => console.log(res));
+  }, []);
+
   return (
     <Provider store={store}>
       <View>
