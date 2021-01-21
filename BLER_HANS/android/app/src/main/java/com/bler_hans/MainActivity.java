a463040525b47414150a921db2dc3e8709c65406
seo2im <seo2im6492@gmail.com>
2021-01-21 14:39:28 +0900

    Init react-native

---
+++ BLER_HANS/android/app/src/main/java/com/bler_hans/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bler_hans;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "BLER_HANS";
+  }
+}
---
+++ BLER_HANS/srcs/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>BL:ER HANS</Text>
+    </View>
+  )
+}
+
+export default App;
